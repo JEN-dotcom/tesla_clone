fcaa166c645b72414bbdcd16afbed7ad9246c262
JEN-dotcom <johneninwa@gmail.com>
2022-08-04 15:03:10 +0100

    Updates

---
+++ precache-manifest.6ffb65e9a3607e1dfd6610545370706d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7281203a100b35cfbf1b",
+    "url": "/tesla_clone/static/css/main.954898a7.chunk.css"
+  },
+  {
+    "revision": "7281203a100b35cfbf1b",
+    "url": "/tesla_clone/static/js/main.7281203a.chunk.js"
+  },
+  {
+    "revision": "e2702f8006e761ebe2ed",
+    "url": "/tesla_clone/static/js/1.e2702f80.chunk.js"
+  },
+  {
+    "revision": "85441e59bf09ede28762",
+    "url": "/tesla_clone/static/js/runtime~main.85441e59.js"
+  },
+  {
+    "revision": "b9bc7903fd454d50d5fdac668a993f9d",
+    "url": "/tesla_clone/index.html"
+  }
+];
